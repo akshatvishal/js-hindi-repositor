cf99598f3e12ec05323c9fde892981d2682b53e0
akshatvishal <155295140+akshatvishal@users.noreply.github.com>
2024-02-20 10:44:02 +0530

    Create 03_conversionoperators.js

---
+++ Basics/03_conversionoperators.js
@@ -0,0 +1,24 @@
+let score = "33";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+// "33" => 33
+// "33abe" => NaN
+// ture => 1 ; false=> 0;
+
+let isloggedin= "ajfn";
+let variableboolean= Boolean(isloggedin)
+console.log(variableboolean)
+
+// 1 => true; 0 => false
+// "" => false ; "string"=>true
+
+let somenumber = 33;
+let isstring = String(somenumber);
+console.log(isstring);
+console.log(typeof isstring)
