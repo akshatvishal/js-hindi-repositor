79b0cd846c391ecbf1e7ebbc48f038b3af0c7148
akshatvishal <155295140+akshatvishal@users.noreply.github.com>
2024-02-20 14:33:58 +0530

    Create 04_comparison.js

---
+++ Basics/04_comparison.js
@@ -0,0 +1,21 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1) //return true
+console.log("02">6) //retuern false
+
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null <= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2"==2); //return true 
+console.log("2"===2); //return false
