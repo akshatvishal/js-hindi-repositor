731527541bcef6a5f3c20b2ed8d01c4ad55e21fa
akshatvishal <155295140+akshatvishal@users.noreply.github.com>
2024-02-20 10:39:20 +0530

    Create 01_Variables.js

---
+++ Basics/01_Variables.js
@@ -0,0 +1,6 @@
+const accountId = "akshat";
+var accountCity= "Lucknow";
+let accountPassword= "12345";
+accountEmail="akshat@gmail.com";
+
+console.table([accountId , accountCity , accountPassword , accountEmail])
