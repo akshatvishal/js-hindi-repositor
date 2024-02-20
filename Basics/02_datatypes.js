b0de3f5d45d84c3c6d6818033a70dbddf8ea2273
akshatvishal <155295140+akshatvishal@users.noreply.github.com>
2024-02-20 10:39:40 +0530

    Create 02_datatypes.js

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS cose as newer version
+// alert(3+3); we are using nodejs , not browser
+console.log(3+3);
+console.log("hitesh");
+let age= 18
+let isloggedin = true
+let state 
+// PRIMITIVE DATATYPE
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null standalone value
+// undefined =>
+// sybmbol => unique
+
+// object
+console.log(typeof age);
+console.log(typeof state);
