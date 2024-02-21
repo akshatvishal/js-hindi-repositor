179e9580124034bf542977e77ab4a2bef7418afb
akshatvishal <155295140+akshatvishal@users.noreply.github.com>
2024-02-21 14:36:18 +0530

    Add files via upload

---
+++ Basics/05_string.js
@@ -0,0 +1,23 @@
+const name = "akshat"
+const repocount = 50
+
+// console.log(name + repocount+ " value") this is a bad and outfated method
+
+// console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+
+const gamename = new String(`akshat`)
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(3));
+// console.log(gamename.charAt(3));
+//  console.log(gamename.charAt(3));
+
+// const newstring= gamename.substring(1,5)
+// console.log(newstring);
+const newerstring= gamename.slice(-4,5)
+console.log(newerstring);
+
+
